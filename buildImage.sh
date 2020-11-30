6fa85ba50085d7aed478ce447a3f1d81a9ec29fc
star <star@funpodium.net>
2020-11-30 11:35:49 +0800

    add buidImage.sh run.sh

---
+++ buildImage.sh
@@ -0,0 +1,3 @@
+docker build -t samualpay/add_one_club-backend:latest .
+docker login --username=samualpay
+docker push samualpay/add_one_club-backend:latest
---
+++ run.sh
@@ -0,0 +1,2 @@
+docker rmi samualpay/add_one_club-backend:latest
+docker-compose up -d
