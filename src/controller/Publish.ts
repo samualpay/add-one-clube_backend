959394908d5bd3033df76d2c65266674249a6fb6
star <star@funpodium.net>
2020-12-17 09:05:28 +0800

    add url for publish response

--- src/controller/Publish.ts
+++ src/controller/Publish.ts
@@ -8,10 +8,20 @@ import publishRepository from "../repository/Publish";
 import { Publish } from "../entity/Publish";
 import HttpException from "../exception/HttpException";
 import { ActivityStatus } from "../enum/ActivityStatus";
+const publishMobilePage =
+  process.env.PUBLISH_MOBILE_PAGE || "http://localhost:3000/mobile/publish";
 type CreateProps = {
   activityId: number;
   machineId: number;
 };
+
+function addUrlForPublish(publish: Publish) {
+  let url = `${publishMobilePage}/${publish.id}`;
+  return { url, ...publish };
+}
+function addUrlForPublishs(publishs: Publish[]) {
+  return publishs.map((publish) => addUrlForPublish(publish));
+}
 class PublishController extends BaseController {
   public path = "/api/publishs";
   initRoutes(): Rout[] {
@@ -51,17 +61,19 @@ class PublishController extends BaseController {
 
   private async findByActivityId(req: Request, res: Response) {
     let activityId: number = parseInt(req.params.id);
-    let list: Publish[] = await publishRepository.findWithRelation({
+    let publishs: Publish[] = await publishRepository.findWithRelation({
       activityId,
     });
+    let list = addUrlForPublishs(publishs);
     res.json({ list });
   }
 
   private async findByMachineId(req: Request, res: Response) {
     let machineId: number = parseInt(req.params.id);
-    let list: Publish[] = await publishRepository.findWithRelation({
+    let publishs: Publish[] = await publishRepository.findWithRelation({
       machineId,
     });
+    let list = addUrlForPublishs(publishs);
     res.json({ list });
   }
   private async create(req: Request, res: Response) {
@@ -87,7 +99,8 @@ class PublishController extends BaseController {
       activityId,
       machineId,
     });
-    let result = await publishRepository.save(entity);
+    publish = await publishRepository.save(entity);
+    let result = addUrlForPublish(publish);
     res.json(result);
   }
   private async delete(req: Request, res: Response) {
@@ -115,7 +128,8 @@ class PublishController extends BaseController {
       throw new HttpException(403, "Permission denied");
     }
     publishEntity.publish = publish;
-    let result = await publishRepository.save(publishEntity);
+    publishEntity = await publishRepository.save(publishEntity);
+    let result = addUrlForPublish(publishEntity);
     res.json(result);
   }
 }
